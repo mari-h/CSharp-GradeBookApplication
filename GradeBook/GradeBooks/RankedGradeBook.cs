63238e2120bf8882671f558bc1214863a5284f41
mari-h <marihaland@hotmail.com>
2018-02-20 13:08:55 +0100

    StandardGradeBook+RankedGradeBook_v2

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -7,7 +7,7 @@ namespace GradeBook.GradeBooks
 {
     class RankedGradeBook:BaseGradeBook
     {
-        public RankedGradeBook(string name) : base(name)
+        RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
         }
--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using GradeBook.Enums;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-    class StandardGradeBook
+    internal class StandardGradeBook: BaseGradeBook
     {
+        StandardGradeBook(string name):base(name)
+        {
+            Type = GradeBookType.Standard;
+
+
+        }
+
+    
+    
     }
 }
